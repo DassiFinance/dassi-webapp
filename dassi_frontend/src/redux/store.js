2b385607c9f6999de5c5f0ed93e15308f59b3819
rutug <rutugaglani@gmail.com>
2021-08-12 15:26:02 +0530

    initial react-redux setup

---
+++ dassi_frontend/src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const reducer = combineReducers({});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+export default store;
