46cc127453df527085848b8f32cf64ec8f33dd4f
rutug <rutugaglani@gmail.com>
2021-08-31 15:03:15 +0530

    Designed connect btn in sidenav

--- dassi_frontend/src/components/wallet/connectBtn.js
+++ dassi_frontend/src/components/wallet/connectBtn.js
@@ -1,4 +1,9 @@
-import { FormControlLabel, Switch, Tooltip } from "@material-ui/core";
+import {
+  FormControlLabel,
+  Switch,
+  Tooltip,
+  makeStyles,
+} from "@material-ui/core";
 
 import {
   WalletConnectButton as MaterialUIWalletConnectButton,
@@ -22,7 +27,89 @@ import React, { useCallback, useMemo } from "react";
 import RequestAirdrop from "./requestAirdrop";
 import SendTransaction from "./sendTransaction";
 
+const useStyles = makeStyles((theme) => ({
+  multiBtn: {
+    backgroundColor: "#BBF146 !important",
+    color: "#15202B !important",
+    fontWeight: "500",
+    fontSize: "15px",
+    textAlign: "center",
+    width: "100% !important",
+  },
+  disconnectBtn: {
+    backgroundColor: "#15202B !important",
+    color: "#BBF146 !important",
+    fontWeight: "500",
+    fontSize: "15px",
+    textAlign: "center",
+    border: "2px solid #BBF146 !important",
+    width: "100% !important",
+    "&.Mui-disabled": {
+      display: "none !important",
+    },
+  },
+  root: {
+    "& .MuiDialog-paper": {
+      width: theme.spacing(40),
+      margin: 0,
+    },
+    "& .MuiDialogTitle-root": {
+      backgroundColor: "#BBF146",
+      "& .MuiTypography-root": {
+        display: "flex",
+        justifyContent: "space-between",
+        lineHeight: theme.spacing(5) + "px",
+      },
+      "& .MuiIconButton-root": {
+        flexShrink: 1,
+        padding: theme.spacing(),
+        marginRight: theme.spacing(-1),
+        color: "#15202B",
+      },
+    },
+    "& .MuiDialogContent-root": {
+      padding: 0,
+      "& .MuiCollapse-root": {
+        "& .MuiList-root": {
+          background: "#15202B",
+        },
+      },
+      "& .MuiList-root": {
+        background: "#15202B",
+        padding: 0,
+      },
+      "& .MuiListItem-root": {
+        boxShadow: "inset 0 1px 0 0 " + "rgba(255, 255, 255, 0.1)",
+        "&:hover": {
+          boxShadow:
+            "inset 0 1px 0 0 " +
+            "rgba(255, 255, 255, 0.1)" +
+            ", 0 1px 0 0 " +
+            "rgba(255, 255, 255, 0.05)",
+        },
+        padding: 0,
+        "& .MuiButton-endIcon": {
+          margin: 0,
+        },
+        "& .MuiButton-root": {
+          flexGrow: 1,
+          justifyContent: "space-between",
+          padding: theme.spacing(1, 3),
+          borderRadius: undefined,
+          fontSize: "1rem",
+          fontWeight: 400,
+          color: "#fff",
+        },
+        "& .MuiSvgIcon-root": {
+          color: "#15202B",
+        },
+      },
+    },
+  },
+}));
+
 const Wallet = () => {
+  const classes = useStyles();
   const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
   const [autoConnect, setAutoConnect] = useLocalStorage("autoConnect", true);
   const wallets = useMemo(() => [getPhantomWallet(), getSolletWallet()], []);
@@ -46,12 +133,14 @@ const Wallet = () => {
         autoConnect={autoConnect}
       >
         {console.log(endpoint)}
-        <MaterialUIWalletDialogProvider>
+        <MaterialUIWalletDialogProvider className={classes.root}>
           <div style={{ marginTop: "5%" }}>
-            <MaterialUIWalletMultiButton />
+            <MaterialUIWalletMultiButton className={classes.multiBtn} />
           </div>
           <div style={{ marginTop: "5%" }}>
-            <MaterialUIWalletDisconnectButton />
+            <MaterialUIWalletDisconnectButton
+              className={classes.disconnectBtn}
+            />
           </div>
           <div style={{ marginTop: "5%" }}>
             {" "}
